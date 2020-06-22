c48fccc65a6a84c6864b6eae7e63c54f354f3a8b
Sebastian Galli <sebastian.galli@outlook.com>
2020-06-22 18:24:09 +0200

    Add Anaconda3 to Setup

--- Infrastructure/Azure/DeploySoftware.ps1
+++ Infrastructure/Azure/DeploySoftware.ps1
@@ -10,3 +10,9 @@ $wc.Downloadfile("$URL", "$DestinationFolder\$File")
 
 # Extract Carla
 Expand-Archive -LiteralPath "$DestinationFolder\$File" -DestinationPath "$DestinationFolder"
+
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install Anaconda
+choco install anaconda3 -y
